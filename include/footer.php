cf11fa890a234aac5a1def40f597d64aa53197ae
slearn3 <43135821+slearn3@users.noreply.github.com>
2020-03-30 10:52:06 +0530

    adding include files

---
+++ include/footer.php
@@ -0,0 +1,142 @@
+<!-- Footer -->
+<footer class="page-footer font-small mdb-color pt-4 bg-dark">
+
+    <!-- Footer Links -->
+    <div class="container text-center text-md-left text-white">
+  
+      <!-- Footer links -->
+      <div class="row text-center text-md-left mt-3 pb-3">
+  
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+          <h6 class="text-uppercase mb-4 font-weight-bold">Bethel Tower Ministry</h6>
+          <p class="text-justify">
+              Bethel Tower is a Prophetic ministry which is founded by Man of God G.Murali Moses. It is a ministry where preaching and teaching about the kingdom of God, healings, miracles, prophecies and deliverances are performed in the mighty name of Jesus Christ.
+          </p>
+        </div>
+        <!-- Grid column -->
+  
+        <hr class="w-100 clearfix d-md-none">
+  
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3 color-change">
+          <h6 class="text-uppercase mb-4 font-weight-bold">Products</h6>
+          <p>
+            <a href="http://ecohomeslanka.com/" class="text-white">Centrium</a>
+          </p>
+          <p>
+            <a href="http://ecohomeslanka.com/" class="text-white">Wind Crest</a>
+          </p>
+          <!--<p>
+            <a href="#!"></a>
+          </p>
+          <p>
+            <a href=""></a>
+          </p> -->
+        </div>
+        <!-- Grid column -->
+  
+        <hr class="w-100 clearfix d-md-none">
+  
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3 color-change">
+          <h6 class="text-uppercase mb-4 font-weight-bold">Useful links</h6>
+          <p >
+            <a href="#" class="text-white">More</a>
+          </p>
+          <p>
+            <a href="#" class="text-white">Daily Prophecy</a>
+          </p>
+          <p>
+            <a href="#" class="text-white">Personal Appointment</a>
+          </p>
+          <p>
+            <a href="#" class="text-white">Help</a>
+          </p>
+        </div>
+  
+        <!-- Grid column -->
+        <hr class="w-100 clearfix d-md-none">
+  
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+          <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
+          <p>
+            <i class="fa fa-home mr-3"></i> Bethel Tower Church,
+            Chilukala Building,
+            Shanthi Nagar Colony,
+            Balaji Nagar, Yapral,
+            Hyderabad, Telangana State,
+            India.</p>
+          <p>
+            <i class="fa fa-envelope mr-3"></i>info@gmmoses.org </p>
+          <p>
+            <i class="fa fa-phone mr-3"></i>91 9908020772</p>
+          
+        </div>
+        <!-- Grid column -->
+  
+      </div>
+      <!-- Footer links -->
+  
+      <hr>
+  
+      <!-- Grid row -->
+      <div class="row d-flex align-items-center">
+  
+        <!-- Grid column -->
+        <div class="col-md-7 col-lg-8">
+  
+          <!--Copyright-->
+          <p class="text-center text-md-left">© 2019 Copyright:
+            <a href="moses.com" target="_blank">
+              <strong>Moses</strong>
+            </a>
+          </p>
+  
+        </div>
+        <!-- Grid column -->
+  
+        <!-- Grid column -->
+        <div class="col-md-5 col-lg-4 ml-lg-0">
+  
+          <!-- Social buttons -->
+          <div class="text-center text-md-right">
+            <ul class="list-unstyled list-inline color-change">
+              <li class="list-inline-item">
+                <a href="#" class="btn-floating btn-sm rgba-white-slight mx-1">
+                  <i class="fab fa-facebook"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="#" class="btn-floating btn-sm rgba-white-slight mx-1">
+                  <i class="fab fa-twitter"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="#"class="btn-floating btn-sm rgba-white-slight mx-1">
+                  <i class="fab fa-google"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="#" class="btn-floating btn-sm rgba-white-slight mx-1">
+                  <i class="fab fa-linkedin"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+  
+        </div>
+        <!-- Grid column -->
+  
+      </div>
+      <!-- Grid row -->
+  
+    </div>
+    <!-- Footer Links -->
+  
+  </footer>
+  <!-- Footer -->
+
+  </body>
+</html>
---
+++ include/header.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title> GM Moses </title>
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--bootstrap css 4.1.3-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <!--style sheet-->
+    <link rel="stylesheet" href="css/style.css">
+    <!--font awsome-->
+    <script src="https://kit.fontawesome.com/fc3a84f5b6.js" crossorigin="anonymous"></script>
+
+    <!--bootstrap jquery-->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <!--javascript-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="js/script.js"></script>
+	<script src="././js/validation.js"></script>
+    <!--animate-->
+    <link href="css/animate.css" rel="stylesheet"/>
+    <!--google fonts-->
+    <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
+    <link href='https://fonts.googleapis.com/css?family=Alef' rel='stylesheet'>
+    <link href='https://fonts.googleapis.com/css?family=Berkshire Swash' rel='stylesheet'>
+  <style> 
+  body{
+    font-family: roboto;
+  }
+  h4,h3{
+    font-family:Alef;
+  }
+  .logo-font{
+    font-family:Berkshire Swash;
+  }
+  </style>
+  <script src="js/wow.min.js"></script>
+  <script>
+     new WOW().init();
+  </script>
+  </head>
+  <body>
+    <!--header-->
+    <div class="header" style="background-image: url(img/bg/bgc.jpg); background-repeat:no-repeat;background-position:center;background-color:black;">
+     
+      <div class="container">
+        <!--nav bar-->
+        <nav class="navbar navbar-expand-md navbar-dark ">
+          <a class="navbar-brand display-4" href="index.php"><h4 class="logo-font">Bethel Tower Ministries</h4></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse justify-content-lg-end" id="collapsibleNavbar">
+            <ul class="navbar-nav float-lg-right">
+            <li class="nav-item nav-menu">
+                <a class="nav-link text-white" href="index.php">Home</a>
+              </li>
+              <li class="nav-item  about-menu">
+                <a class="nav-link text-white" href="#">About</a>
+                <div class="sub-menu bg-white pr-4 pl-4 pt-2 pb-1">
+                    <a class="dropdown-item wow fadeInUp animated slower dealy-2s" href="about-ministry.php">Our Ministry</a>
+                    <a class="dropdown-item wow fadeInUp animated slower dealy-4s" href="about-church.php">Church</a>
+                    <a class="dropdown-item wow fadeInUp animated slower dealy-8s" href="prayer-request.php">Prayer Request</a>
+                </div>
+
+              </li>
+              <li class="nav-item nav-menu">
+                <a class="nav-link text-white" href="events.php">Events</a>
+              </li>
+              <li class="nav-item nav-menu">
+                <a class="nav-link text-white" href="sermons.php">Sermons</a>
+              </li>
+              <li class="nav-item nav-menu">
+                  <a class="nav-link text-white" href="contactus.php">Contact Us</a>
+                </li>
+              <li class="nav-item nav-menu-donate">
+                  <a class="nav-link border pr-4 pl-4 " href="donate.php">Donate</a>
+                </li>   
+            </ul>
+          </div>  
+        </nav>
+        <!--/nav bar-->
